1c2f1e3a583524fda79704a64df3ca91c99e9aa5
Tiziano Cialfi <tiziano.cialfi@soft.it>
2023-03-16 15:11:54 +0100

    Initial Commit

---
+++ LocationFinder/LocationFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationFinderApp.swift
+//  LocationFinder
+//
+//  Created by Tiziano Cialfi on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LocationFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
